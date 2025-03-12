ed5889ee90076f52c1f8b730d77de65f1cf75366
MarkPreedy <mark_preedy@hotmail.com>
2025-03-12 13:26:54 +0000

    add secret key

--- things2do/settings.py
+++ things2do/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#niynb=l$tvfoj-6zbbtiqq*4*jp^8zf49qni%5a3&5#@!g+1f'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
